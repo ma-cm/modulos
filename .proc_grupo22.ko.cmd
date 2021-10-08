443f0b58a2a8401a313b0ef4be2df5b98c811a67
Mitchel <2740840740101@ingenieria.usac.edu.gt>
2021-10-08 13:49:07 -0600

    d

---
+++ .proc_grupo22.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/mitchel/Documents/SO1/modulos/proceso/proc_grupo22.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/mitchel/Documents/SO1/modulos/proceso/proc_grupo22.ko /home/mitchel/Documents/SO1/modulos/proceso/proc_grupo22.o /home/mitchel/Documents/SO1/modulos/proceso/proc_grupo22.mod.o
---
+++ .tmp_versions/proc_grupo22.mod
@@ -0,0 +1,2 @@
+/home/mitchel/Documents/SO1/modulos/proceso/proc_grupo22.ko
+/home/mitchel/Documents/SO1/modulos/proceso/proc_grupo22.o
